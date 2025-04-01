3d8038d157afa1977ea2953f3f90b5d8f72b4574
Toni Pekkala <toni.pekkala@hotmail.com>
2025-04-01 22:44:19 +0300

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ShoplistMauiBlazorHybrid
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
